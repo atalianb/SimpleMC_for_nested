ac8d855f4ec43fabd3efaae1c0f8d7ea5d19f2d7
atalianb <atalia.navarro.boullosa@gmail.com>
2022-06-14 12:47:42 -0500

    F730-V1_fast_500lp

--- simplemc/likelihoods/RotationCurvesLikelihood.py
+++ simplemc/likelihoods/RotationCurvesLikelihood.py
@@ -41,5 +41,5 @@ class RotationCurvesLikelihood(BaseLikelihood):
 
 class RotationCurvesLike(RotationCurvesLikelihood):
     def __init__(self):
-        RotationCurvesLikelihood.__init__(self,"RotCurves","simplemc/data/data_used_by_Tula/U11748.dat",
-            "simplemc/data/U11748-cov.txt")
+        RotationCurvesLikelihood.__init__(self,"RotCurves","simplemc/data/data_used_by_Tula/F730-V1.dat",
+            "simplemc/data/F730-V1-cov.txt")
